ce9abd3e542020fcad2187dec50999dd0a74b0bc
filiperochs <filipesilva.tmsi@gmail.com>
2023-03-29 10:03:31 -0300

    feat: add currency

--- Franc.java
+++ Franc.java
@@ -1,10 +1,16 @@
 class Franc extends Money {
+  private String currency;
 
   Franc(int amount) {
     this.amount = amount;
+    currency = "CHF";
   }
 
   public Money times(int multiplier) {
     return new Franc(amount * multiplier);
+  }
+
+  String currency() {
+    return currency;
   }
 }
